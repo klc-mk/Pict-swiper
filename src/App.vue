e6cc4366d8af3e13c5263c016021cfee5659ce19
klc-mk <kelcomc@gmail.com>
2022-01-12 13:58:36 +0900

    とりあえずpush

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  <p>
+    <PictSwiper srcA='images/sample_pict_A.jpg' srcB='images/sample_pict_B.jpg'/>
+  </p>
+
+  </div>
+</template>
+
+<script>
+import PictSwiper from './components/pict-swiper.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PictSwiper
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/pict-swiper.vue
@@ -0,0 +1,89 @@
+<template>
+  <div style="position:relative;" ref="parent">
+
+    <div style="position:absolute;top:0px;left;0px;z-index:0;"
+        @mousedown="touchstart($event)"
+        @mousemove="imgdrag($event)"
+        @mouseleave="touchend($event)"
+        @mouseup="touchend($event)"        
+    >
+        <img :src="imageA" ref="imgref">
+        <div style="position:absolute;top:0px;left;0px;" :style="posset">
+            <img class="imgtrim" :src="imageB">
+        </div>
+        <span :style="barheight"/>
+
+    </div>
+   
+  </div>
+
+</template>
+
+<script>
+
+export default {
+    name: 'App',
+    props:['srcA','srcB'],
+    data:function(){
+        return{
+            clicked:false,
+            xpos:20,
+            size:{width:0,height:0},
+        }
+    },
+    mounted:function(){
+        this.size.height = this.$refs.imgref.naturalHeight;
+        this.size.width = this.$refs.imgref.naturalWidth;     
+    },
+    computed:{
+        imageA:function(){
+            return require("../"+this.srcA);
+        },
+        imageB:function(){
+            return require("../"+this.srcB);
+        },
+        posset:function(){ 
+            return {width:this.xpos+'px',height:this.size.height+'px'};
+        },
+        barheight:function(){
+            return {width:'4px',height:this.size.height+'px',
+            position:'absolute',
+            top:'0px',
+            left:this.xpos+'px',
+            background: '#000000',
+            'z-index':'2'
+            };
+        }
+    },
+    methods:{
+        touchstart:function(e){
+            this.clicked = true;
+            this.xpos = e.clientX - this.$refs.parent.getBoundingClientRect().left;
+
+        },
+        imgdrag:function(e){
+            if(this.clicked){
+                this.xpos = e.clientX - this.$refs.parent.getBoundingClientRect().left;
+            }
+        },
+        touchend:function(){
+            this.clicked = false;
+        },
+    }
+}
+</script>
+
+<style>
+    img{
+        pointer-events: none;
+        user-select: none;
+        -webkit-user-drag: none;
+        -moz-user-select: none;
+    }
+    .imgtrim{
+        width:100%;
+        height:100%;
+        object-fit: cover;
+        object-position:0 0%;
+    }
+</style>
